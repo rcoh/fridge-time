66e00629efe910b86692a921ea10ade438c28a51
Russell Cohen <russell.r.cohen@gmail.com>
2023-12-05 13:56:13 -0500

    Add icon

--- app/src/main/java/com/example/fridgetime/MainActivity.kt
+++ app/src/main/java/com/example/fridgetime/MainActivity.kt
@@ -8,12 +8,19 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -22,15 +29,25 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+sealed class State(val message: String) {
+    object AppStartup : State("App is starting...")
+    object Connecting : State("Connecting to printer")
+    object Connected : State("Printer is connected!")
+    data class ConnectFailed(val error: String) : State("Connect failed ($error)")
+}
 
 class MainActivity : ComponentActivity() {
     private val bluetoothScanner = BluetoothScanner(this)
-    private var model: Model = Model(null, "app is starting...", true)
+    private var model: Model = Model(null, state = State.AppStartup)
 
     private fun updateModel(update: (Model) -> Model) {
         this.model = update(this.model)
         runOnUiThread {
-            setContent { ui(this.model) }
+            setContent { Ui(this.model) }
         }
     }
 
@@ -38,9 +55,9 @@ class MainActivity : ComponentActivity() {
     @SuppressLint("MissingPermission")
     suspend fun findDevice(deviceName: String): BluetoothDevice? {
         return suspendCoroutine { continuation ->
-            bluetoothScanner.bondedDevices {
-                continuation.resume(it.find {
-                    it.name.startsWith(
+            bluetoothScanner.bondedDevices { devices ->
+                continuation.resume(devices.find { device ->
+                    device.name.startsWith(
                         deviceName
                     )
                 })
@@ -48,7 +65,6 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         updateModel { x -> x }
@@ -58,20 +74,22 @@ class MainActivity : ComponentActivity() {
     }
 
     private suspend fun connectPrinter() {
-        updateModel { model -> model.copy(connecting = true, message = "connecting to printer")  }
+        updateModel { model -> model.copy(state = State.Connecting) }
         val bonded = findDevice("D110")
-        if (bonded == null) {
-            updateModel { it.copy(message = "No printer currently connected", connecting = false) }
+        val state = if (bonded == null) {
+            State.ConnectFailed("Please pair a printer")
         } else {
             val client =
                 NiimbotPrinterClient(bonded.address, bluetoothScanner.bluetoothAdapter!!)
             if (!client.connected()) {
-                updateModel { it.copy(message = "found printer, but not connected", connecting = false) }
+                State.ConnectFailed("printer paired but not currently connected")
             } else {
                 val printer = DatePrinter(client)
-                updateModel { it.copy(printer = printer, connecting = false, message = "ready to print") }
+                updateModel { it.copy(printer = printer, state = State.Connected) }
+                return
             }
         }
+        updateModel { it.copy(state = state) }
     }
 
     private fun printToday(printer: DatePrinter): () -> Unit {
@@ -81,77 +99,103 @@ class MainActivity : ComponentActivity() {
     private fun printTomorrow(printer: DatePrinter): () -> Unit {
         return { lifecycleScope.launch { printer.printTomorrow() } }
     }
-    /*
-        runOnUiThread {
-            setContent { Text("app is starting") }
-        }
-        lifecycleScope.launch {
-            println("looking for device...")
-            val bonded = findDevice("D110")
-            println("found device")
 
-            if (bonded == null)  {
-                runOnUiThread { setContent { Text("no bonded clients") } }
-            } else {
-                runOnUiThread { setContent {
-                    Button(onClick = {
-                        lifecycleScope.launch {
-                            val client = withContext(Dispatchers.IO) { NiimbotPrinterClient(bonded.address, bluetoothScanner.bluetoothAdapter!!) }
-                            if (!client.connected()) {
-                                runOnUiThread { setContent { Text("printer not connected") } }
-                            } else {
-                                val hb = withContext(Dispatchers.IO) {
-                                    DatePrinter(client).printSomething()
-                                }
-                                println("battery life: $hb")
-                                runOnUiThread { setContent { Text("got heart beat!, $hb") } }
-                            }
-                        }
-                    }) { Text("print!")}
-                } }
-            }
-        }
-        return
-    }*/
+    private fun printText(printer: DatePrinter, text: String): () -> Unit {
+        return { lifecycleScope.launch { printer.printText(text) } }
+    }
 
-    /*
-        bluetoothScanner.startScanning {
-            println("callback hit...")
-            val printer = it.filter { (it.name ?: "").startsWith("D110") }.firstOrNull()
-            runOnUiThread {
-                if (printer == null) {
-                    setContent { Text("printer not found (${it.map { it.name }})") }
-                } else {
-                    setContent { Text("found printer! ${printer.address}, ${printer.bondState}") }
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    private fun Ui(model: Model) {
+        var customText by remember { mutableStateOf("") }
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            // Printing buttons row
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 8.dp),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Button(onClick = model.printer?.let { printToday(it) } ?: {},
+                    enabled = model.printer != null
+                ) {
+                    Text("Print Today")
+                }
+
+                Button(onClick = model.printer?.let { printTomorrow(it) } ?: {},
+                    enabled = model.printer != null
+                ) {
+                    Text("Print Tomorrow")
                 }
             }
-            if (printer != null) {
-                val client =
-                    NiimbotPrinterClient(printer.address, bluetoothScanner.bluetoothAdapter!!)
-                suspend {
-                    val hb = client.heartbeat()
-                    runOnUiThread { setContent { Text("got heart beat!, $hb") } }
+
+            // Divider for visual separation
+            Divider(modifier = Modifier.padding(vertical = 8.dp))
+
+            // Connection status and button row
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 8.dp),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                val text = if (model.connecting()) "Connecting..." else "Reconnect Printer"
+                Button(onClick = {
+                    lifecycleScope.launch(Dispatchers.IO) {
+                        connectPrinter()
+                    }
+                }, enabled = !model.connecting()) {
+                    Text(text = text)
                 }
             }
-            /*runOnUiThread {
-                setContent {
-                    Text(it.map { it.name }.toString())
+
+            // Status message
+            Text(
+                text = model.state.message,
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            // Custom text input and print button
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 8.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                TextField(
+                    value = customText,
+                    onValueChange = { customText = it },
+                    label = { Text("Enter Custom Text") },
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                Button(onClick = model.printer?.let { printText(it, customText) } ?: {},
+                    enabled = model.printer != null && !model.connecting()
+                ) {
+                    Text("Print Custom Text")
                 }
-            }*/
+            }
+
         }
-}*/
+    }
+
 
     @Composable
-    private fun ui(model: Model) {
-        val message = model.message
+    private fun UiOld(model: Model) {
         return Column(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(16.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            // Static label to display the message
-
             // Row to align buttons horizontally
             Row(
                 modifier = Modifier.fillMaxWidth(),
@@ -176,30 +220,28 @@ class MainActivity : ComponentActivity() {
                 modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceEvenly
             ) {
-                if (model.printer == null && !model.connecting) {
-                    Button(onClick = {
-                        lifecycleScope.launch(Dispatchers.IO) {
-                            connectPrinter()
-                        }
-                    }) {
-                        Text(text = "Reconnect printer")
+                val text = when (model.connecting()) {
+                    true -> "Connecting..."
+                    false -> "Reconnect printer"
+                }
+                Button(onClick = {
+                    lifecycleScope.launch(Dispatchers.IO) {
+                        connectPrinter()
                     }
+                }, enabled = !model.connecting()) {
+                    Text(text = text)
                 }
             }
 
             Text(
-                text = message,
+                text = model.state.message,
                 //style = MaterialTheme.typography.displayMedium,
                 modifier = Modifier.padding(bottom = 16.dp)
             )
         }
     }
-
-
-    override fun onPause() {
-        super.onPause()
-        bluetoothScanner.stopScanning()
-    }
 }
 
-data class Model(val printer: DatePrinter?, val message: String, val connecting: Boolean)
+data class Model(val printer: DatePrinter? = null, val message: String? = null, val state: State) {
+    fun connecting(): Boolean = state == State.Connecting
+}
